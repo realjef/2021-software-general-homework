2ed6ab0e6bb178d2179c4bf5b5124e2cbc784ce3
Kate U <kmoomie123@gmail.com>
2021-08-22 18:34:12 -0700

    Add homework files

---
+++ lesson_1.py
@@ -0,0 +1,8 @@
+# Create a program in which the user creates 
+# a specific FRC team and store the following variables:
+	# Team Number (named team_number)
+	# Team Name (named team_name)
+	# Location (named location)
+	# Rookie Year (named rookie_year)
+	# Is Active (named is_active)
+# Be sure to store each variable as the correct type!
---
+++ lesson_2.py
@@ -0,0 +1,22 @@
+# A student has taken 3 tests in a class, and wants to know their current grade 
+# (which is only calculated by these tests). 
+
+# Ask the user to input all three of the test scores for the student, one by one. 
+
+# The program should then calculate the average test score (average is adding all three 
+# test scores together then dividing by 3), and then print the student's letter grade 
+# (as well as the average score as a number).
+
+
+
+
+#########################################################
+
+# Gregory wants to know how many toys they can buy at Toys'N'Us
+
+# They prioritize buying the most expensive toys first (For ejm. If Gregory had $50 
+# they'd end up buying 2 Jumbo Baby Yoda Plushies, 1 Beyblade, and 5 Sticky Hands with 
+# a remainder of $0.30 left)
+
+# Have the user input how much money Gregory has then print how many of each 
+# toy they can afford, as well as how much money they'd have remaining
